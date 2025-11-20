733b7b2e6920df4b1674035106a5329276393a9a
Irene Ferfoglia <irene.ferfo@gmail.com>
2025-11-20 14:51:25 +0100

    updated to match saving metrics, adaptation for task arrays

--- tests/architecture_variations.py
+++ tests/architecture_variations.py
@@ -1,8 +1,8 @@
 import os
-from dataclasses import dataclass
+from dataclasses import dataclass, replace
 
 from STELLE.data.dataset_loader import get_dataset
-from STELLE.explanations.explanation_utils import compute_explanations
+from STELLE.explanations.explanation_utils import compute_explanations, load_cached_metrics, save_metrics
 from STELLE.kernels.kernel_utils import set_kernels_and_concepts
 from STELLE.model.model_utils import train_test_model
 from STELLE.utils import (
@@ -36,19 +36,19 @@ class ExperimentConfig:
     cf: int = 300
     patience: int = 10
     val_every_n_epochs: int = 1
-    verbose: int = 100
+    verbose: int = 20
     logging: bool = False
 
     # Kernel parameters
     normalize_kernel: bool = False
     exp_kernel: bool = False
-    normalize_rhotau: bool = True
-    exp_rhotau: bool = True
+    normalize_rhotau: bool = False
+    exp_rhotau: bool = False
 
     # Concept parameters
     t: float = 1.0
     nvars_formulae: int = 1
-    creation_mode: str = "one"
+    creation_mode: str = "all"
     dim_concepts: int = 1000
     min_total: int = 100
     imp_t_l: float = 0
@@ -78,7 +78,6 @@ def get_anchor_items(
     from STELLE.kernels.kernel_utils import _validate_parameters
 
     """Initialize kernels and generate concepts for STL-based learning."""
-
     # Validate and adjust parameters
     _, creation_mode = _validate_parameters(
         config.nvars, config.nvars_formulae, config.creation_mode
@@ -107,7 +106,9 @@ def get_anchor_items(
 
 
 def main():
+    global dim_anchors
     args = parse_arguments()
+    task_id = os.environ.get("SLURM_ARRAY_TASK_ID")
     config = ExperimentConfig()
     _ = setup_environment(config.seed)
     base_path = "tests/results/architecture_variations/"
@@ -129,59 +130,89 @@ def main():
     kernel, formula_manager, concepts_time = set_kernels_and_concepts(
         trainloader.dataset, paths["phis_path_og"], config
     )
-
-    for arch_type in ["Anchor", "Robs", "RobsAsHx", "RobsAsGx", "NoGx", "base"]:
-        print(
-            f"\n>>>>>>>>>>>>>>>>>>>>> arch_type = {arch_type} >>>>>>>>>>>>>>>>>>>>>\n"
-        )
-        model_id = (
-            f"seed_{config.seed}_{config.lr}_{config.init_crel}_{config.init_eps}_{config.h}_"
-            f"{config.n_layers}_bs{config.bs}_{arch_type}"
-        )
-        # attach to model for later reference and debugging
-        print(f"Model ID: {model_id}")
-        model_path_ev = os.path.join(paths["model_path_og"], f"{model_id}.pt")
-
-        args = (kernel, trainloader, valloader, testloader, model_path_ev, config)
-        if arch_type == "Anchor":
-            base_concepts, concepts, concept_embeddings, base_concepts_time = (
-                get_anchor_items(
-                    kernel,
-                    trainloader.dataset,
-                    paths["phis_path_og"],
-                    formula_manager,
-                    dim_anchors,
-                    config,
-                )
-            )
-            model, accuracy_results = train_test_model(
-                args,
-                arch_type=arch_type,
-                concepts=concepts,
-                concept_embeddings=concept_embeddings,
-            )
-        else:
-            base_concepts_time = 0
-            model, accuracy_results = train_test_model(args, arch_type=arch_type)
-
-        args_explanations = (model_path_ev, trainloader, testloader, model, config)
-
-        local_metrics, global_metrics = compute_explanations(args_explanations)
-
-        result_raw = merge_result_dicts(
-            [accuracy_results, local_metrics, global_metrics]
-        )
-
-        result = {
-            "arch_type": arch_type,
-            "concepts_time": concepts_time + base_concepts_time,
-            **result_raw,
+    
+    archs_list = {
+        1: ["Anchor", "Robs"],
+        2: ["RobsAsHx", "RobsAsGx"],
+        3: ["NoGx", "base"]
         }
 
-        result = flatten_dict(result)
-        results.append(result)
+    if task_id is None:
+        # run all architectures in all groups
+        archs = sum(archs_list.values(), [])
+    else:
+        archs = archs_list[task_id]
+    for arch_type in archs:
+        print(
+            f"\n\n>>>>>>>>>>>>>>>>>>>>> arch_type = {arch_type} >>>>>>>>>>>>>>>>>>>>>"
+        )
+        for lr in [1e-4, 1e-5, 1e-6]: 
+            print(
+                f"\n>>>>>>>>>>>>>>>>>>>>> lr = {lr} >>>>>>>>>>>>>>>>>>>>>\n"
+            )
+            config = replace(config, lr=lr)
+            model_id = (
+                f"seed_{config.seed}_{config.lr}_{config.init_crel}_{config.init_eps}_{config.h}_"
+                f"{config.n_layers}_bs{config.bs}_{arch_type}"
+            )
+            # attach to model for later reference and debugging
+            print(f"Model ID: {model_id}")
+            model_path_ev = os.path.join(paths["model_path_og"], f"{model_id}.pt")
 
-        save_results(results, paths["results_dir"])
+            args = (kernel, trainloader, valloader, testloader, model_path_ev, config)
+            if arch_type == "Anchor":
+                _, concepts, concept_embeddings, base_concepts_time = (
+                    get_anchor_items(
+                        kernel,
+                        trainloader.dataset,
+                        paths["phis_path_og"],
+                        formula_manager,
+                        dim_anchors,
+                        config,
+                    )
+                )
+                model, accuracy_results = train_test_model(
+                    args,
+                    arch_type=arch_type,
+                    concepts=concepts,
+                    concept_embeddings=concept_embeddings,
+                )
+            else:
+                base_concepts_time = dim_anchors = 0
+                model, accuracy_results = train_test_model(args, arch_type=arch_type)
+
+            # Check for cached metrics first
+            local_metrics, global_metrics = load_cached_metrics(model_path_ev, config)
+        
+            if local_metrics is None:
+                args_explanations = (
+                    model_path_ev,
+                    trainloader,
+                    testloader,
+                    model,
+                    config,
+                )
+                local_metrics, global_metrics = compute_explanations(args_explanations, save=False)
+                save_metrics(model_path_ev, config, local_metrics, global_metrics)
+
+            result_raw = merge_result_dicts(
+                [accuracy_results, local_metrics, global_metrics]
+            )
+
+            result = {
+                "arch_type": arch_type,
+                "concepts_time": concepts_time + base_concepts_time,
+                "dim_anchors": dim_anchors,
+                "lr": config.lr, 
+                **result_raw,
+            }
+
+            result = flatten_dict(result)
+            results.append(result)
+
+            save_results(results, paths["results_dir"])
+            
+            del model
 
 
 if __name__ == "__main__":
